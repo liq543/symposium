3dad9263b51a29c754b2dc7163a2771828954129
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-09-06 13:19:04 -0500

    Added server.js to server directory, added comments as needed

---
+++ server/server.js
@@ -0,0 +1,52 @@
+// Requiring Express, the Apollo Server for GraphQL, and the authentication middleware
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+// Requiring the schemas and connecting to our database
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+// Establishes our port, starts the Apollo server, and passes in the schemas and authentication
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+// Creates directory for final build
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// Serves the index.html file to the client
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+  
+// Call the async function to start the server
+startApolloServer();
