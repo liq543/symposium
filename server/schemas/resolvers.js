7b8fb7620e10a717c39e1845222145b91d820519
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-09-07 15:49:13 -0500

    Updated Queries in resolvers.js based on typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,93 +1,50 @@
+// Requires the AuthenticationError from Apollo, our models, and the sign token
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Playlist, Song } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
+// Establishes all resolvers based on the typeDefs in greater detail
 const resolvers = {
   Query: {
-    categories: async () => {
-      return await Category.find();
+    // Finds a user by its ID, which should be run when a user successfully logs in
+    user: async (parent, { _id }) => {
+      return await User.findById(_id);
     },
-    products: async (parent, { category, name }) => {
+
+    // Finds all playlists that the user has created so far, which should also be run when a user successfully logs in
+    playlists: async () => {
+      return await Playlist.find();
+    },
+
+    // Find a specific playlist by its ID and populates all songs that are in the playlist, which should be run when a user clicks on one of their playlists
+    playlist: async (parent, { _id }) => {
+      return await Playlist.findById(_id).populate('songs');
+    },
+
+    // Find all songs by title and/or artist and/or name, which should be run when a user navigates to the Search Page, enters this information in the Search Query component, and clicks on the Search button
+    songs: async (parent, { title, artist, album }) => {
       const params = {};
 
-      if (category) {
-        params.category = category;
+      if (title) {
+        params.title = title;
       }
 
-      if (name) {
-        params.name = {
-          $regex: name
-        };
+      if (artist) {
+        params.artist = artist;
       }
 
-      return await Product.find(params).populate('category');
+      if (album) {
+        params.album = album;
+      }
+
+      return await Song.find(params);
     },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+
+    // Find a song by its ID, which should be run when a user clicks on a song from either one of their playlists or from their search results
+    song: async (parent, { _id }) => {
+      return await Song.findById(_id);
     },
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-        return user;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    order: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-
-        return user.orders.id(_id);
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
-
-      const { products } = await order.populate('products');
-
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
-        });
-
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
-
-        line_items.push({
-          price: price.id,
-          quantity: 1
-        });
-      }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
-
-      return { session: session.id };
-    }
   },
   Mutation: {
     addUser: async (parent, args) => {
