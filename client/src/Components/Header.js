84e19111011fd8ab9320a65c8ffb0440aa559697
unknown <liq543@gmail.com>
2023-09-07 14:22:25 -0500

    spotify is now linked

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
+const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+const REDIRECT_URI = 'http://localhost:3000/callback';
 
+const loginURL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private&response_type=token&state=123`;
 const Header = () => (
     <div className="py-5 px-8 flex justify-between items-center border-b" style={{ borderColor: '#4F518C' }}>
         <div className="flex items-center">
@@ -7,7 +11,7 @@ const Header = () => (
             <h1 className="text-3xl font-bold -ml-2" style={{ color: '#DABFFF' }}>ymposium</h1>
         </div>
         <div className="space-x-4">
-            <button className="px-5 py-2 rounded-full hover:bg-DABFFF" style={{ backgroundColor: '#907AD6' }}>Login</button>
+        <a href={loginURL} className="px-5 py-2 rounded-full hover:bg-DABFFF" style={{ backgroundColor: '#907AD6' }}>Login</a>
             <button className="px-5 py-2 rounded-full hover:bg-DABFFF" style={{ backgroundColor: '#907AD6' }}>Sign Up</button>
         </div>
     </div>
